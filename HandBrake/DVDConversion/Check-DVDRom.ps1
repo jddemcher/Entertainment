fab2b675eb58118f1724bbdd4ee5b486dcec14ab
jddemcher@gmail.com <jddemcher@gmail.com>
2020-02-24 20:52:23 -0500

    cleanup

--- HandBrake/DVDConversion/Check-DVDRom.ps1
+++
@@ -1,7 +0,0 @@
-Do {
-    $MediaLoaded = (get-wmiobject -class win32_cdromdrive -Property MediaLoaded).MediaLoaded
-    Start-Sleep -Seconds 15
-}
-Until ($MediaLoaded -eq $true)
-
-Get-ScheduledTask -TaskName 'Convert-DVD' | Start-ScheduledTask
