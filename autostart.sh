bc44460ce8e81f91c89e83317772db954fef4fbf
jddemcher@gmail.com <jddemcher@gmail.com>
2020-03-20 15:26:49 -0400

    cleaned up comments

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
-#array of games
+
 declare -a GAMES
-#GAMES[0]="dkong.zip"
-#GAMES[1]="frogger.zip"
-#GAMES[2]="galaga.zip"
-#GAMES[3]="joust.zip"
-#GAMES[4]="mspacman.zip"
-#GAMES[5]="pacman.zip"
-#GAMES[6]="mario.zip"
-#GAMES[7]="qbert.zip"
-#GAMES[8]="contra.zip"
-#GAMES[9]="asteroid.zip"
 
 for file in /home/pi/RetroPie/roms/mame-libretro/*.zip
 do
@@ -20,4 +10,6 @@ done
 #random game selection
 play=${GAMES[$RANDOM % ${#GAMES[@]} ]}
 /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ "mame-libretro" "$HOME/RetroPie/roms/mame-libretro/$play"
+
+#start emulation after hotkey breakout of the game that was autoloaded
 emulationstation #auto
