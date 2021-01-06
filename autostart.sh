80497e518f8701ee374fa0fefb5e3729e6310618
jddemcher <jddemcher@geisinger.edu>
2021-01-06 08:21:04 -0500

    added comments

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
-
+# this script can be configured to run in the location:
+#
+#     /opt/retropie/configs/all/autostart.sh
+#
+# I developed this script to auto start a game randomly in a given location, this one specifically
+# runs any of my MAME roms
 declare -a GAMES
 
 for file in /home/pi/RetroPie/roms/mame-libretro/*.zip
